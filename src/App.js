aa998349cf89e8d1aa1f6493ad2dd99c4f1f0d81
Ian Be <ian84be@gmail.com>
2019-10-01 14:26:52 -0500

    scaffold NavBar & styles, remove dead code, fix security vuln

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, NavLink } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App__header">
-        <div className="NavBar"></div>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App__header">
+          <div className="NavBar">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </div>
+        </header>
+        <Switch>
+          <main>
+            <p>main content</p>
+          </main>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
